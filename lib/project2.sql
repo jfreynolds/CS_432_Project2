788278bcad674f5159f07a0fb1f1f48e6981c5ae
John <jreynolds99639@gmail.com>
2017-11-09 18:35:45 -0500

    Start script unpopulated

--- lib/project2.sql
+++ lib/project2.sql
@@ -1,9 +1,47 @@
---Run script from project1 and this projct to create all necessary tables
+--Run script to create all necessary tables
+--TODO: Populate Tables
+
+--Create sequences (Question #1)
+create sequence pur#_seq
+start with 100000
+maxvalue 999999
+increment by 1;
+
+create sequence sup#_seq
+start with 1000
+maxvalue 9999
+increment by 1;
+
+create sequence log#_seq
+start with 10000
+maxvalue 99999
+increment by 1;
+
+drop table customers;
+drop table discounts;
+drop table employees;
+drop table logs;
 drop table products;
+drop table purchases;
+drop table suppliers;
+drop table supplies;
+
+create table customers
+(cid char(4) primary key,
+name varchar2(15),
+telephone# char(12),
+visits_made number(4) check (visits_made >= 1),
+last_visit_date date);
 
 create table discounts
 (discnt_category number(1) primary key check(discnt_category in (1, 2, 3, 4)),
 discnt_rate number(3,2) check (discnt_rate between 0 and 0.8));
+
+create table employees
+(eid char(3) primary key,
+name varchar2(15),
+telephone# char(12),
+email varchar2(20));
 
 create table products
 (pid char(4) primary key,
@@ -12,6 +50,15 @@ qoh number(5),
 qoh_threshold number(4),
 original_price number(6,2),
 discnt_category number(1) references discounts);
+
+create table purchases
+(pur# number(6) primary key,
+eid char(3) references employees(eid),
+pid char(4) references products(pid),
+cid char(4) references customers(cid),
+qty number(5),
+ptime date,
+total_price number(7,2));
 
 create table suppliers
 (sid char(2) primary key,
