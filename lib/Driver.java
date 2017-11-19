9e922cc34b6ae9fe2d347a19897c61b1400ef1e1
johnNull <nullj27@gmail.com>
2017-11-19 16:15:56 -0500

    q4 and q5, starting on interface

--- lib/Driver.java
+++ lib/Driver.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+import java.io.Console;
+
 public class Driver{
 	public static void main(String args[]){
-		instructions inst = new instructions();
-		inst.showTable("customers");
-		inst.showTable("purchases");
+		Scanner sc = new Scanner(System.in);
+		Console con = System.console();
+		System.out.println("enter username:");
+		String user = sc.nextLine();
+		String pass = new String(con.readPassword("enter password:\n"));
+		instructions inst = new instructions(user, pass);
+
+		/*inst.showTable("customers");
+		System.out.println(inst.showTable("purchases"));
 		System.out.println(inst.getSavings("100000"));
+		System.out.println(inst.getSaleActivity("e01"));
+		inst.addCustomer("e03", "firt mcgee", "607-888-8888");
+		System.out.println(inst.showTable("customers"));*/
 	}
 }
--- lib/instructions.java
+++ lib/instructions.java
@@ -8,18 +8,18 @@ import oracle.jdbc.pool.OracleDataSource;
 public class instructions{
 	OracleDataSource ds;
 	Connection conn;
-	public instructions(){
+	public instructions(String user, String pass){
 		try{
 			ds = new oracle.jdbc.pool.OracleDataSource();
 			System.out.println("Attempting to connect...");
 			ds.setURL("jdbc:oracle:thin:@castor.cc.binghamton.edu:1521:acad111");
-			conn = ds.getConnection("jnull1", "95987Vcs");
+			conn = ds.getConnection(user, pass);
 		}
 		catch (SQLException ex) { System.out.println ("\n*** SQLException caught ***\n" + ex.getMessage());}
    		catch (Exception e) {System.out.println ("\n*** other Exception caught ***\n");}
 	}
 
-	public void showTable(String tblname){
+	public String showTable(String tblname){
 		try{
 			CallableStatement cs = conn.prepareCall("begin ? := instructions.showTable(?); end;");
 			cs.setString(2, tblname);
@@ -28,24 +28,27 @@ public class instructions{
 			ResultSet rs = (ResultSet)cs.getObject(1);
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int numColumns = rsmd.getColumnCount();
+			String output = "";
 
 			// print the results
 			for(int i = 1; i <= numColumns; i++){
-				System.out.format("%-20s", rsmd.getColumnName(i) + " ");
+				output += String.format("%-20s", rsmd.getColumnName(i) + " ");
 			}
-			System.out.println();
+			output += "\n";
 			while (rs.next()) {
 				for(int i = 1; i <= numColumns; i++){
-					System.out.format("%-20s", rs.getString(i) + " ");
+					output += String.format("%-20s", rs.getString(i) + " ");
 				}
-				System.out.println();
+				output += "\n";
 			}
 
 			//close the result set, statement, and the connection
 			cs.close();
+			return output;
 		}
 		catch (SQLException ex) { System.out.println ("\n*** SQLException caught ***\n" + ex.getMessage());}
    		catch (Exception e) {System.out.println ("\n*** other Exception caught ***\n");}
+		return "-1";
 	}
 
 	public double getSavings(String pur){
@@ -61,5 +64,49 @@ public class instructions{
 		catch (SQLException ex) { System.out.println ("\n*** SQLException caught ***\n" + ex.getMessage());}
    		catch (Exception e) {System.out.println ("\n*** other Exception caught ***\n");}
 		return -1;
+	}
+
+	public String getSaleActivity(String id){
+		try{
+			String sql = "begin instructions.monthly_sale_activities(?, ?); end;";
+			CallableStatement cs = conn.prepareCall(sql);
+			cs.setString(1, id);
+			cs.registerOutParameter(2, OracleTypes.CURSOR);
+			cs.execute();
+			ResultSet rs = (ResultSet)cs.getObject(2);
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int numColumns = rsmd.getColumnCount();
+			String output = "";
+			// print the results
+			for(int i = 1; i <= numColumns; i++){
+				output += String.format("%-20s", rsmd.getColumnName(i) + " ");
+			}
+			output += "\n";
+			while (rs.next()) {
+				for(int i = 1; i <= numColumns; i++){
+					output += String.format("%-20s", rs.getString(i) + " ");
+				}
+				output += "\n";
+			}
+			cs.close();
+			return output;
+		}
+		catch (SQLException ex) { System.out.println ("\n*** SQLException caught ***\n" + ex.getMessage());}
+   		catch (Exception e) {System.out.println ("\n*** other Exception caught ***\n");}
+		return "-1";
+	}
+
+	public void addCustomer(String cid, String name, String telephone){
+		try{
+			String sql = "begin instructions.add_customer(?, ?, ?); end;";
+			CallableStatement cs = conn.prepareCall(sql);
+			cs.setString(1, cid);
+			cs.setString(2, name);
+			cs.setString(3, telephone);
+			cs.execute();
+			cs.close();
+		}
+		catch (SQLException ex) { System.out.println ("\n*** SQLException caught ***\n" + ex.getMessage());}
+   		catch (Exception e) {System.out.println ("\n*** other Exception caught ***\n");}
 	}
 }
